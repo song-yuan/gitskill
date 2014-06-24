bac10da1fcbe6d61b612fc3f2e3d45f8fa47886d
zuoshutong <2015064545@qq.com>
2014-06-24 13:12:32 +0800

    replace

--- index.php
+++ index.php
@@ -11,26 +11,26 @@
 
 <span class="code">git init</span>
 <span class="disc">添加文件</span>
-<span class="code">git add <fileName></span>
+<span class="code">git add [fileName]</span>
 <span class="disc">提交修改到版本库：</span>
 <span class="code">git commit -m "discription"></span>
 
 <span class="disc">检出</span>
 <span class="code">git checkout</span>
-<span class="code">git checkout -- <fileName></span>
+<span class="code">git checkout -- [fileName]</span>
 <span class="disc">切换分支</span>
-<span class="code">git checkout <branchName></span>
-<span class="code">git checkout -b <branchName></span>
-<span class="code">git checkout -b <branchName> <remote/branchName></span>
+<span class="code">git checkout [branchName]</span>
+<span class="code">git checkout -b [branchName]</span>
+<span class="code">git checkout -b [branchName] <remote/branchName]</span>
 
 
 <span class="code">git branch</span>
-<span class="code">git branch <branchName></span>
-<span class="code">git branch --set-upstream <branchName> <remote/branchName></span>
-<span class="code">git merge <branchName></span>
+<span class="code">git branch [branchName]</span>
+<span class="code">git branch --set-upstream [branchName] [remote/branchName]</span>
+<span class="code">git merge [branchName]</span>
 <span class="code">git remote</span>
 <span class="code">git remote -v</span>
-<span class="code">git push origin <branchName></span>
+<span class="code">git push origin [branchName]</span>
 <span class="code">git pull</span>
 
 
