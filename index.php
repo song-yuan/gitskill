a70bd6170e84184e35b67a0afceab977534043c6
zuoshutong <2015064545@qq.com>
2014-06-24 10:35:22 +0800

    modify home page

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-	echo "Home Page!";
-?>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<style>
+		span.code{display:block;padding:10px;margin:10px 0;border:1px dashed #ccc;}
+		span.disc{display:block;}
+	</style>
+</head>
+<body>
+<h1 style="font-size:14px;">some git command</h1>
+<span class="code">git add < fileName ></span>
+<span class="disc">添加文件</span>
+
+<span class="code">git commit -m "discription"></span>
+<span class="code">git add < fileName ></span>
+<span class="code">git add < fileName ></span>
+</body>
+</html>
